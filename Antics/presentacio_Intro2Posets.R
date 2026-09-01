7ce8e95708eb35e6733d50ed6a5c2171493fb27e
Francesc <francescmartori@gmail.com>
2026-09-01 16:00:36 +0200

    cleanup

--- Antics/presentacio_Intro2Posets.R
+++
@@ -1,6 +0,0 @@
-a <- data.frame(PIB=c(75000, 75000, 50000, 40000, 40000), 
-                EV = c(80,75, 60, 70, 65))
-rownames(a)<- c("P1","P2","P3","P4","P5")
-Z <- get_poset(a, rownames(a))
-
-plot(Z)
